8698607cc4c7c1920092cf4fd47fd4d15c810e38
komalup1991 <upadhyay.ko@northeastern.edu>
2023-07-13 15:02:24 -0700

    Add create services to manifest

---
+++ PawsomePals/app/src/main/java/edu/northeastern/pawsomepals/ui/feed/CreateRecipeActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.pawsomepals.ui.feed;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import edu.northeastern.pawsomepals.R;
+
+public class CreateRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recipe);
+    }
+}
+
