972529c535282839aadf1e9cc293d55d83385751
Ruby-8 <lziyang004@gmail.com>
2023-07-18 15:41:09 -0700

    Update edit chat room info function.

--- PawsomePals/app/src/main/java/edu/northeastern/pawsomepals/ui/chat/ChatRoomActivity.java
+++ PawsomePals/app/src/main/java/edu/northeastern/pawsomepals/ui/chat/ChatRoomActivity.java
@@ -3,6 +3,7 @@ package edu.northeastern.pawsomepals.ui.chat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -20,6 +21,7 @@ public class ChatRoomActivity extends AppCompatActivity {
     ImageButton sendMessageBtn;
     ImageButton functionBtn;
     ImageButton backBtn;
+    ImageButton infoBtn;
     TextView otherUserName;
     RecyclerView chatRoomRecyclerView;
 
@@ -39,6 +41,7 @@ public class ChatRoomActivity extends AppCompatActivity {
         messageInput = findViewById(R.id.message_input);
         sendMessageBtn = findViewById(R.id.message_send_btn);
         functionBtn = findViewById(R.id.function_btn);
+        infoBtn = findViewById(R.id.chatroom_more_button);
         chatRoomRecyclerView = findViewById(R.id.message_recycler_view);
         backBtn = findViewById(R.id.message_back_button);
 
@@ -46,6 +49,15 @@ public class ChatRoomActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check
+                Intent intent = new Intent(getApplicationContext(),EditChatRoomInfoActivity.class);
+                startActivity(intent);
             }
         });
 
