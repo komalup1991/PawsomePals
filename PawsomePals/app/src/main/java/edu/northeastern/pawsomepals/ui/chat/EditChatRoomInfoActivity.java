bb26854b3eddcf46d4f0c9ef349dd168e4359405
Ruby-8 <lziyang004@gmail.com>
2023-07-18 15:41:17 -0700

    Update edit chat room info function.

---
+++ PawsomePals/app/src/main/java/edu/northeastern/pawsomepals/ui/chat/EditChatRoomInfoActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.pawsomepals.ui.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.pawsomepals.R;
+
+public class EditChatRoomInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_chat_room_info);
+    }
+}
