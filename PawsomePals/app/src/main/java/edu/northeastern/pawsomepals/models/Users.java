824fd774930a04f74d6cf84d16b9ff0d376421ef
komalup1991 <upadhyay.ko@northeastern.edu>
2023-07-18 20:23:48 -0700

    Add user details to firestore only for Username verification for now

---
+++ PawsomePals/app/src/main/java/edu/northeastern/pawsomepals/models/Users.java
@@ -0,0 +1,42 @@
+package edu.northeastern.pawsomepals.models;
+
+public class Users {
+
+    private String userId;
+    private String email;
+    private String name;
+    private String recipeId;
+
+    public Users() {
+    }
+
+    public Users(String userId,String email){
+        this.userId = userId;
+        this.email = email;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(String recipeId) {
+        this.recipeId = recipeId;
+    }
+}
--- PawsomePals/app/src/main/java/edu/northeastern/pawsomepals/ui/login/SignUpActivity.java
+++ PawsomePals/app/src/main/java/edu/northeastern/pawsomepals/ui/login/SignUpActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -15,13 +16,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.northeastern.pawsomepals.R;
+import edu.northeastern.pawsomepals.models.Users;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -88,6 +96,7 @@ public class SignUpActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
+                        createUserDetails(email);
                         progressBar.setVisibility(View.GONE);
 
                         Toast.makeText(SignUpActivity.this, R.string.signup_successful_registration, Toast.LENGTH_SHORT).show();
@@ -103,6 +112,37 @@ public class SignUpActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void createUserDetails(String email) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        assert currentUser != null;
+        String loggedInUserId = currentUser.getUid();
+
+        Users user = new Users(loggedInUserId,email);
+        Map<String,Object> userInfo = new HashMap<>();
+        userInfo.put("userId",user.getUserId());
+        userInfo.put("email",user.getEmail());
+        db.collection("user")
+                .add(userInfo)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("Create User", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Create User", "Error adding document", e);
+                    }
+                });
+
+
+
     }
 
     @Override
