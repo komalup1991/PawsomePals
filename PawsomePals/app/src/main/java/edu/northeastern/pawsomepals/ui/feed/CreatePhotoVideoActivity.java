165b95a5610890f81aee16ce648d0e7543ec50f0
komalup1991 <upadhyay.ko@northeastern.edu>
2023-07-22 23:32:56 -0700

    Add UI for other feed creation

--- PawsomePals/app/src/main/java/edu/northeastern/pawsomepals/ui/feed/CreatePhotoVideoActivity.java
+++ PawsomePals/app/src/main/java/edu/northeastern/pawsomepals/ui/feed/CreatePhotoVideoActivity.java
@@ -1,16 +1,30 @@
 package edu.northeastern.pawsomepals.ui.feed;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
 
 import edu.northeastern.pawsomepals.R;
 
 public class CreatePhotoVideoActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_photo_video);
+
+    toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Add Photo Video");
+        }
+
     }
 }
